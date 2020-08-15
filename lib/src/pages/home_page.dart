c15efd2c3f23308a770e978b137d6d261defe54f
XanderJames1910 <xanderjames1910@gmail.com>
2020-08-14 22:27:01 -0500

    Swiper Peliculas App

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+// Local Imports
+import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Películas en Cine'),
+          backgroundColor: Colors.indigoAccent,
+          actions: [IconButton(icon: Icon(Icons.search), onPressed: () {})],
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              _swiperTarjetas(),
+            ],
+          ),
+        ));
+  }
+
+  Widget _swiperTarjetas() {
+    return CardSwiper(
+      peliculas: [1,2,3,4,5],
+    );
+  }
+}
